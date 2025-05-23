c365260fd6cad7f8d2fcb1fe5e570b19b8987ae2
Kigula Jesse <jkigula@icloud.com>
2025-05-23 09:01:56 +0300

    Deepseek

--- deepseek.py
+++ deepseek.py
@@ -6,5 +6,7 @@ class MLA(nn.Module):
 
         Attributes:
         dim (int): Dimensionality of the input features.
-        n_heads (int): Number of attention heads 
+        n_heads (int): Number of attention heads.
+        n_local_heads (int): Number of local attention heads for distributed systems
+        q_lora_rank (int): Rank for low-rank query projection
     """
